53d109065f41506a9213e63dcc134dccf24c72f6
Eng Jehad Almaliki <deeppp901@gmail.com>
2022-02-26 23:05:13 +0300

    CURD Experince

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ const { Router } = require('express');
 const assert = require("assert")
 const multer = require('multer');
 const path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 const User = require('./../models/user');
 const Skills = require('./../models/dash-skills');
+var experienceModel = require('../models/Experince');
+var qualificationsModel=require('../models/qualifications');
 // ===multer file==//
-
+require('dotenv/config');
 const storage = multer.diskStorage({
  
+
 });
 
 const upload = multer({
@@ -34,11 +39,18 @@ router.get('/dash-social', function(req, res) {
 router.get('/dash-Edu', function(req, res) {
   res.render('pages/dash-Eduction');
 }); 
-// Experince page
-router.get('/dash-Experince', function(req, res) {
-  res.render('pages/dash-Experince');
-}); 
-
+router.get('/dash-Edu', function(req, res, next) {
+  qualificationsModel.find().then((result)=>{
+  
+    res.render('pages/dash-Eduction', { qualifications:result });
+  })
+  });
+// GET Expirence 
+router.get('/dash-Experince', (req, res, next)=>{
+  experienceModel.find().then((result) =>{
+    res.render('pages/dash-Experince', { data: result})
+  })
+})
 // 
 // Skills page
 router.get('/dash-Skill', function(req, res, next) {
@@ -92,7 +104,7 @@ router.post('/edit_User', function(req, res, next){
   res.redirect('/dashboard');
 });
 
-//Delete experience item
+//Delete user item
 
 router.get('/delete_user/:id',function(req,res,next){
   User.deleteOne({"_id":req.params.id},function(err,result){
@@ -140,6 +152,55 @@ router.get('/delete_skill/:id',function(req,res,next){
     console.log("item deleted");
   })
   res.redirect('/dash-Skill');
+
+});
+
+//Add new expirince to the view in the data tables section
+
+router.post('/add_experience', function(req, res, next) {
+     
+  var experienceDetails = new experienceModel({
+    experience: req.body.experience,
+    year: req.body.year,
+    company_name: req.body.company_name,
+    Description: req.body.Description,
+        Start_Date: req.body.Start_Date,
+        End_Date: req.body.End_Date,
+  });
+   
+  experienceDetails .save();
+        
+
+    res.redirect('/dash-Experince');
+
+});
+// Edit expirince
+//Edit  experience on the view in the data tables section
+
+router.post('/edit_experience', function(req, res, next){
+  var item = {
+    experience: req.body.experience,
+    year: req.body.year,
+    company_name:req.body.company_name,
+    Description: req.body.Description,
+    Start_Date: req.body.Start_Date,
+    End_Date: req.body.End_Date
+  };
+  var id = req.body.id;
+  experienceModel.updateOne({"_id": id}, {$set: item}, item, function(err, result){
+    assert.equal(null, err);
+    console.log("item updated");
+  })
+  res.redirect('/dash-Experince');
+});
+
+//Delete experience item
+
+router.get('/delete_experience/:id',function(req,res,next){
+  experienceModel.deleteOne({"_id":req.params.id},function(err,result){
+    console.log("item deleted");
+  })
+res.redirect('/dash-Experince');
 
 });
 module.exports = router;
