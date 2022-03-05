4b68ba6033109dd6fe8b7e9a8df59544590be4be
Eng Jehad Almaliki <deeppp901@gmail.com>
2022-03-05 11:50:35 +0300

    Complete signup process

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -11,5 +11,26 @@ passport.use('local.signup',new localStrategy({
 },(req,username,password,done)=>{
     if(req.body.password !=req.body.confirm_password){
         return done(null,false,req.flash('error','Password is error'))
+    }else {
+        Userlogin.findOne({email:username},(err,user)=>{
+            if(err){
+                return done(err)
+            }
+            if(user){
+                return done(null,false,req.flash('error','Email already used')) 
+            }
+            if (!user){
+                let newUser=new  Userlogin()
+                newUser.email=req.body.email
+                newUser.password=newUser.hashPassword(req.body.password)
+                newUser.save((err,user)=>{
+                    if(!err){
+                        return done(null,false,req.flash('success','User Added'))  
+                    }else{
+                        console.log(err)
+                    }
+                })
+            }
+        })
     }
 }))
